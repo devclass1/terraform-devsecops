df4384e1469ae11d00e05c384e7fbe0f0a7faab3
devclass1 <72622124+devclass1@users.noreply.github.com>
2023-04-24 11:27:32 +0530

    Create variables.tf

---
+++ container-registry/variables.tf
@@ -0,0 +1,11 @@
+variable "prefix" {
+  type        = string
+  default = "test94557584"
+  description = "The prefix used for all resources in this example"
+}
+
+variable "location" {
+  type        = string
+  default = "East US 2"
+  description = "The Azure location where all resources in this example should be created"
+}
