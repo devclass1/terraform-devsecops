c984abc3d1637b5aa198e027280441d5818336a6
devclass1 <72622124+devclass1@users.noreply.github.com>
2023-09-13 08:40:54 +0530

    Create variables.tf

---
+++ vnet/variables.tf
@@ -0,0 +1,8 @@
+variable "rg_name" {
+    description = "Define Your Resource Group name here"
+    default = "class10"
+}
+variable "rg_location" {
+    description = "Define Your Resource Group location here"
+    default = "East US2"
+}
